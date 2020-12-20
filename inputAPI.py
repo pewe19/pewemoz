5a2719404dee8d5cb7b566f999a98efc8b49feed
pewe <49721279+pewe19@users.noreply.github.com>
2020-12-20 10:15:12 +0700

    Add files via upload

---
+++ inputAPI.py
@@ -0,0 +1,31 @@
+import json
+print("PeweMoz V.1.1")
+print("Gunakan API MOZ Anda Sendiri!!")
+print("  ")
+print("    ____                   __  __")
+print("   |  _ \ _____      _____|  \/  | ___ ____")
+print("   | |_) / _ \ \ /\ / / _ \ |\/| |/ _ \_  /")
+print("   |  __/  __/\ V  V /  __/ |  | | (_) / /")
+print("   |_|   \___| \_/\_/ \___|_|  |_|\___/___|")
+print("  ")
+print("  ")
+uid = input("API MOZ Anda: ")
+passwd = input("API Secret MOZ Anda: ")
+print("  ")
+print("  ")
+try:
+    x = open("config.json").read()
+    x = json.loads(x)
+    x['id'].append(uid)
+    x['pass'].append(passwd)
+    x['creditsused'].append('0')
+    with open("config.json", "w+") as f:
+            x = str(x)
+            x = x.replace("'", "\"")
+            f.write(x)
+            f.close()
+    print("Berhasil menambahkan API keys ke dalam list." + "\n" + "\n" + uid + "\n" + "\n"+ passwd + "\n")
+except FileNotFoundError:
+    print("Error encountered! Please check if config.json file exists.")
+
+
---
+++ reset.py
@@ -0,0 +1,36 @@
+print("PeweMoz V.1.1")
+print("Gunakan API MOZ Anda Sendiri!!")
+print("  ")
+print("    ____                   __  __")
+print("   |  _ \ _____      _____|  \/  | ___ ____")
+print("   | |_) / _ \ \ /\ / / _ \ |\/| |/ _ \_  /")
+print("   |  __/  __/\ V  V /  __/ |  | | (_) / /")
+print("   |_|   \___| \_/\_/ \___|_|  |_|\___/___|")
+print("  ")
+print("  ")
+answer = input("Anda ingin reset API keys data Anda ? Input ya or tidak: ") 
+if answer == "ya": 
+    file1 = open("config.json","w") 
+    L = ["{\"id\": [""], \"pass\": [""], \"creditsused\": [""]}"]
+    file1.writelines(L) 
+    file1.close()
+    print("  ")
+    print("  ")
+    print("API Keys sudah di reset mas/mbak bro!!")
+    print("  ")
+    print("  ")
+    watxt = input("Sekalian gak mau hapus file daleman list URLs txt nya? Input ya or tidak: ")
+    if watxt == "ya":
+        file2 = open("urls.txt","w")
+        btxt = []
+        file2.writelines(btxt) 
+        file2.close()
+        print("  ")
+        print("  ")
+        print("List URLs udin di kosongin mas/mbak bro")
+        print("  ")
+        print("  ")
+elif answer == "tidak": 
+    print("oke gak jadi ye..")
+else: 
+    print("Inputnya ya atau tidak, jangan yang lain.")
